40fa8df8f413a023c05f45fc388554025635a5ba
Emma Daniels <e.e.daniels1@uu.nl>
2023-12-05 14:39:12 +0100

    update, small changes

--- virtualship.py
+++ virtualship.py
@@ -1,8 +1,10 @@
 import numpy as np
+import xarray as xr
 import pyproj
 from datetime import timedelta
 from shapely.geometry import Point, Polygon
 from parcels import Field, FieldSet, JITParticle, Variable, ParticleSet
+from scipy.ndimage import uniform_filter1d
 
 __all__ = ['VirtualShip']
 
@@ -56,6 +58,7 @@ class VirtualShip:
             endlat = coords_input[i+1][1]
 
             # calculate line string along path with segments every 5 min = 3.6*60*5 = 1080 m (or switch comment for 2 midpoints)
+            # assumption: ship speed is 3.6 m/s (13 km/h)
             g = pyproj.Geod(ellps='WGS84')
             r = g.inv_intermediate(startlong, startlat, endlong, endlat, del_s = 1080)
             # r = g.inv_intermediate(startlong, startlat, endlong, endlat, initial_idx=0, return_back_azimuth=False, npts=3)
@@ -178,8 +181,46 @@ class VirtualShip:
             total_time += adcp_dt
             pset.time_nextloop[:] = total_time
         # write the final locations of the ADCP particles
-        pset.execute(SampleVel, dt=adcp_dt, runtime=1)
+        pset.execute(SampleVel, dt=adcp_dt, runtime=1, verbose_progress=False)
         adcp_output_file.write_latest_locations(pset, time=total_time)
 
 
+    def postprocess(ctd):
 
+        # rewrite CTD data to cvs
+        for i in range(0, ctd+2):
+            
+            # Open output and read to x, y, z
+            ds = xr.open_zarr(f"./results/CTD_test_{i+1}.zarr")
+            x = ds["lon"][:].squeeze()
+            y = ds["lat"][:].squeeze()
+            z = ds["z"][:].squeeze()
+            time = ds["time"][:].squeeze()
+            T = ds["temperature"][:].squeeze()
+            S = ds["salinity"][:].squeeze()
+            ds.close()
+
+            # add some noise
+            random_walk = np.random.random()/10
+            z_norm = (z-np.min(z))/(np.max(z)-np.min(z))
+            t_norm = np.linspace(0, 1, num=len(time))
+            # dS = abs(np.append(0, np.diff(S))) # scale noise with gradient
+            # for j in range(5, 0, -1):
+            #     dS[dS<1*10**-j] = 0.5-j/10
+            # add smoothed random noise scaled with depth (and OPTIONAL with gradient for S) 
+            # and random (reversed) diversion from initial through time scaled with depth 
+            S = S + uniform_filter1d(
+                np.random.random(S.shape)/5*(1-z_norm) + 
+                random_walk*(np.max(S).values - np.min(S).values)*(1-z_norm)*t_norm/10, 
+                max(int(len(time)/40), 1))
+            T = T + uniform_filter1d(
+                np.random.random(T.shape)*5*(1-z_norm) - 
+                random_walk/2*(np.max(T).values - np.min(T).values)*(1-z_norm)*t_norm/10, 
+                max(int(len(time)/20), 1))
+
+            # reshaping data to export to csv
+            header = f"'pressure [hPa]','temperature [degC]', 'salinity [g kg-1]'"
+            data = np.column_stack([(z/10), T, S])
+            new_line = '\n'
+            np.savetxt(f"./results/CTD_station_{i+1}.csv", data, fmt="%.4f", header=header, delimiter=',', 
+                    comments=f'{x.attrs} {x[0].values}{new_line}{y.attrs}{y[0].values}{new_line}start time: {time[0].values}{new_line}end time: {time[-1].values}{new_line}')
