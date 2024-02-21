dabffd4db4dae1b9a8801f5fe2119081b2f076fc
Emma Daniels <e.e.daniels1@uu.nl>
2024-02-21 10:56:54 +0100

    improving and added postprocessing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Emma's work for the MSc student material
 Please contact me at e.e.daniels1@uu.nl with any questions. 
 
-This is a python tool that will allow students to virtually sample the ocean as if the measurements were coming from an actual oceanographic mission. At the moment we mimic ADCP and CTD measurements and we will likely add argo floats and gliders in the future. 
+This is a python tool that will allow students to virtually sample the ocean as if the measurements were coming from an actual oceanographic mission. At the moment we mimic ADCP, CTD, and simple underwaydata measurements and allow surface drifters and argo float deployments. We might add gliders and meteorological data in the future. 
 
 ### Requirements
 To use the material please create an environment called Parcels with the command
@@ -12,3 +12,6 @@ To use the material please create an environment called Parcels with the command
 The scripts are written to work with A-grid ocean data, specifically that from CMEMS
 Sample instructions for data to be downloaded can be found in 
 `datadownload.py` 
+
+### Sailing the ship
+Fill in the accompanying JSON file and run virtualship.py to start measuring. 
--- virtualship.py
+++ virtualship.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 import xarray as xr
 import pyproj
+import shutil
 from datetime import timedelta
 from shapely.geometry import Point, Polygon
 from scipy.ndimage import uniform_filter1d
@@ -136,10 +137,10 @@ def create_fieldset():
     '''Creates fieldset from netcdf files and adds bathymetry data for CTD cast, returns fieldset with negative depth values'''
     datadirname = os.path.dirname(__file__)
     filenames = {
-        "U": f"{datadirname}/studentdata_UV_klein.nc",
-        "V": f"{datadirname}/studentdata_UV_klein.nc",
-        "S": f"{datadirname}/studentdata_S_klein.nc",
-        "T": f"{datadirname}/studentdata_T_klein.nc"}
+        "U": os.path.join(datadirname, "studentdata_UV_klein.nc"),
+        "V": os.path.join(datadirname, "studentdata_UV_klein.nc"),
+        "S": os.path.join(datadirname, "studentdata_S_klein.nc"),
+        "T": os.path.join(datadirname, "studentdata_T_klein.nc")}
     variables = {'U': 'uo', 'V': 'vo', 'S': 'so', 'T': 'thetao'}
     dimensions = {'lon': 'longitude', 'lat': 'latitude', 'time': 'time', 'depth': 'depth'}
 
@@ -150,9 +151,10 @@ def create_fieldset():
     for g in fieldset.gridset.grids:
         if max(g.depth) > 0:
             g.depth = -g.depth  # make depth negative
+    fieldset.mindepth = -fieldset.U.depth[0]  # uppermost layer in the hydrodynamic data
 
     # add bathymetry data to the fieldset for CTD cast
-    bathymetry_file = f"{datadirname}/GLO-MFC_001_024_mask_bathy.nc"
+    bathymetry_file = os.path.join(datadirname, "GLO-MFC_001_024_mask_bathy.nc")
     bathymetry_variables = ('bathymetry', 'deptho')
     bathymetry_dimensions = {'lon': 'longitude', 'lat': 'latitude'}
     bathymetry_field = Field.from_netcdf(bathymetry_file, bathymetry_variables, bathymetry_dimensions)
@@ -227,11 +229,6 @@ def sailship(config):
     def SampleT(particle, fieldset, time):
         particle.temperature = fieldset.T[time, particle.depth, particle.lat, particle.lon]
 
-    def GracefulFail(particle, fieldset, time):
-        if particle.time > fieldset.maxtime:
-            print("Ship time is over, waiting for drifters and/or Argo floats to finish.")
-            particle.delete()
-
     # Create ADCP like particleset and output file
     ADCP_bins = np.arange(config.ADCP_settings["max_depth"], -5, config.ADCP_settings["bin_size_m"])
     vert_particles = len(ADCP_bins)
@@ -239,7 +236,7 @@ def sailship(config):
         fieldset=fieldset, pclass=VM_ADCPParticle, lon=np.full(vert_particles,sample_lons[0]),
         lat=np.full(vert_particles,sample_lats[0]), depth=ADCP_bins, time=0
     )
-    adcp_output_file = pset_ADCP.ParticleFile(name="./results/sailship_ADCP.zarr")
+    adcp_output_file = pset_ADCP.ParticleFile(name=os.path.join("results","ADCP.zarr"))
     adcp_dt = timedelta(minutes=5).total_seconds() # timestep of ADCP output, every 5 min
 
     # Create underway particle
@@ -247,7 +244,7 @@ def sailship(config):
         fieldset=fieldset, pclass=UnderwayDataParticle, lon=sample_lons[0],
         lat=sample_lats[0], depth=-2, time=0
     )
-    UnderwayData_output_file = pset_UnderwayData.ParticleFile(name="./results/sailship_UnderwayData.zarr")
+    UnderwayData_output_file = pset_UnderwayData.ParticleFile(name=os.path.join("results","UnderwayData.zarr"))
 
     # initialize CTD station number and time
     total_time = timedelta(hours=0).total_seconds()
@@ -278,14 +275,14 @@ def sailship(config):
                 ctd += 1
 
                 # release CTD particle
-                pset_CTD = ParticleSet(fieldset=fieldset, pclass=CTDParticle, lon=sample_lons[i], lat=sample_lats[i],
-                                       depth=-2, time=total_time)
+                pset_CTD = ParticleSet(fieldset=fieldset, pclass=CTDParticle, lon=sample_lons[i], lat=sample_lats[i], depth=fieldset.mindepth, time=total_time)
 
                 # create a ParticleFile to store the CTD output
-                ctd_output_file = pset_CTD.ParticleFile(name=f"./results/CTD_{ctd}.zarr", outputdt=ctd_dt)
+                ctd_output_file = pset_CTD.ParticleFile(name=f"{os.path.join('results','CTDs','CTD_')}{ctd}.zarr", outputdt=ctd_dt)
 
                 # record the temperature and salinity of the particle
-                pset_CTD.execute([SampleS, SampleT, CTDcast], runtime=timedelta(hours=8), dt=ctd_dt, output_file=ctd_output_file, verbose_progress=True)
+                pset_CTD.execute([SampleS, SampleT, CTDcast], runtime=timedelta(hours=8), dt=ctd_dt, 
+                                 output_file=ctd_output_file, verbose_progress=False)
                 total_time = pset_CTD.time[0] + timedelta(minutes=20).total_seconds() # add CTD time and 20 minutes for deployment
 
         # check if we are at a drifter deployment location
@@ -326,7 +323,7 @@ def sailship(config):
     return drifter_time, argo_time
 
 
-def deployments(config, difter_time, argo_time):
+def deployments(config, drifter_time, argo_time):
     '''Deploys drifters and argo floats, returns results folder'''
 
     # TODO update to bigger input files
@@ -337,7 +334,6 @@ def deployments(config, difter_time, argo_time):
     fieldset.add_constant('cycle_days', config.argo_characteristics["cycle_days"])
     fieldset.add_constant('drift_days', config.argo_characteristics["drift_days"])
     fieldset.mindepth = -fieldset.U.depth[0]  # uppermost layer in the hydrodynamic data
-    print(fieldset.mindepth)
 
     # Create particle to sample water underway
     class DrifterParticle(JITParticle):
@@ -420,8 +416,8 @@ def deployments(config, difter_time, argo_time):
         time = drifter_time
 
         # Create and execute drifter particles
-        pset = ParticleSet(fieldset=fieldset, pclass=DrifterParticle, lon=lon, lat=lat, depth=np.zeros(len(time)), time=time)
-        output_file = pset.ParticleFile(name="./results/Drifters.zarr", outputdt=timedelta(hours=1))
+        pset = ParticleSet(fieldset=fieldset, pclass=DrifterParticle, lon=lon, lat=lat, depth=np.repeat(fieldset.mindepth,len(time)), time=time)
+        output_file = pset.ParticleFile(name=os.path.join("results","Drifters.zarr"), outputdt=timedelta(hours=1))
         pset.execute([AdvectionRK4, SampleT, CheckError], runtime=timedelta(weeks=6), dt=timedelta(minutes=5), output_file=output_file)
 
     # initialize argo floats
@@ -434,8 +430,8 @@ def deployments(config, difter_time, argo_time):
         time = argo_time
 
         # Create and execute argo particles
-        argoset = ParticleSet(fieldset=fieldset, pclass=ArgoParticle, lon=lon, lat=lat, depth=np.zeros(len(time)), time=time)
-        argo_output_file = argoset.ParticleFile(name="./results/Argo.zarr", outputdt=timedelta(minutes=5), chunks=(1,500))
+        argoset = ParticleSet(fieldset=fieldset, pclass=ArgoParticle, lon=lon, lat=lat, depth=np.repeat(fieldset.mindepth,len(time)), time=time)
+        argo_output_file = argoset.ParticleFile(name=os.path.join("results","Argos.zarr"), outputdt=timedelta(minutes=5), chunks=(1,500))
         argoset.execute(
             [ArgoVerticalMovement, AdvectionRK4, KeepAtSurface, CheckError],  # list of kernels to be executed
             runtime=timedelta(weeks=6), dt=timedelta(minutes=5),
@@ -444,51 +440,48 @@ def deployments(config, difter_time, argo_time):
 
 
 
-def postprocess(ctd):
+def postprocess():
+    '''Postprocesses CTD data and writes to csv files'''
 
-    # rewrite CTD data to cvs
-    for i in range(1, ctd+1):
+    i = 0
+    for filename in os.scandir(os.path.join("results","CTDs")):
+        # not so happy about using filename and i as they might not be identical
+        if filename.path.endswith(".zarr"):
+            i += 1
+            # Open output and read to x, y, z
+            ds = xr.open_zarr(filename.path)
+            x = ds["lon"][:].squeeze()
+            y = ds["lat"][:].squeeze()
+            z = ds["z"][:].squeeze()
+            time = ds["time"][:].squeeze()
+            T = ds["temperature"][:].squeeze()
+            S = ds["salinity"][:].squeeze()
+            ds.close()
 
-        # Open output and read to x, y, z
-        ds = xr.open_zarr(f"./results/CTD_{i}.zarr")
-        x = ds["lon"][:].squeeze()
-        y = ds["lat"][:].squeeze()
-        z = ds["z"][:].squeeze()
-        time = ds["time"][:].squeeze()
-        T = ds["temperature"][:].squeeze()
-        S = ds["salinity"][:].squeeze()
-        ds.close()
+            random_walk = np.random.random()/10
+            z_norm = (z-np.min(z))/(np.max(z)-np.min(z))
+            t_norm = np.linspace(0, 1, num=len(time))
+            # add smoothed random noise scaled with depth 
+            # and random (reversed) diversion from initial through time scaled with depth
+            S = S + uniform_filter1d(
+                np.random.random(S.shape)/5*(1-z_norm) +
+                random_walk*(np.max(S).values - np.min(S).values)*(1-z_norm)*t_norm/10,
+                max(int(len(time)/40), 1))
+            T = T + uniform_filter1d(
+                np.random.random(T.shape)*5*(1-z_norm) -
+                random_walk/2*(np.max(T).values - np.min(T).values)*(1-z_norm)*t_norm/10,
+                max(int(len(time)/20), 1))
 
-        # add some noise
-        random_walk = np.random.random()/10
-        z_norm = (z-np.min(z))/(np.max(z)-np.min(z))
-        t_norm = np.linspace(0, 1, num=len(time))
-        # dS = abs(np.append(0, np.diff(S))) # scale noise with gradient
-        # for j in range(5, 0, -1):
-        #     dS[dS<1*10**-j] = 0.5-j/10
-        # add smoothed random noise scaled with depth (and OPTIONAL with gradient for S)
-        # and random (reversed) diversion from initial through time scaled with depth
-        S = S + uniform_filter1d(
-            np.random.random(S.shape)/5*(1-z_norm) +
-            random_walk*(np.max(S).values - np.min(S).values)*(1-z_norm)*t_norm/10,
-            max(int(len(time)/40), 1))
-        T = T + uniform_filter1d(
-            np.random.random(T.shape)*5*(1-z_norm) -
-            random_walk/2*(np.max(T).values - np.min(T).values)*(1-z_norm)*t_norm/10,
-            max(int(len(time)/20), 1))
-
-        # reshaping data to export to csv
-        header = f"'pressure [hPa]','temperature [degC]', 'salinity [g kg-1]'"
-        data = np.column_stack([(z/10), T, S])
-        new_line = '\n'
-        np.savetxt(f"./results/CTD_station_{i}.csv", data, fmt="%.4f", header=header, delimiter=',',
-                comments=f'{x.attrs} {x[0].values}{new_line}{y.attrs}{y[0].values}{new_line}start time: {time[0].values}{new_line}end time: {time[-1].values}{new_line}')
-
+            # reshaping data to export to csv
+            header = f"'pressure [hPa]','temperature [degC]', 'salinity [g kg-1]'"
+            data = np.column_stack([(z/10), T, S])
+            new_line = '\n'
+            np.savetxt(f"{os.path.join('results','CTDs','CTD_station_')}{i}.csv", data, fmt="%.4f", header=header, delimiter=',',
+                    comments=f'{x.attrs} {x[0].values}{new_line}{y.attrs}{y[0].values}{new_line}start time: {time[0].values}{new_line}end time: {time[-1].values}{new_line}')
+            # shutil.rmtree(filename.path)
 
 if __name__ == '__main__':
     config = VirtualShipConfiguration('student_input.json')
     drifter_time, argo_time = sailship(config)
-    # # #tmp
-    # drifter_time = []
-    # argo_time = [0, 1]
     deployments(config, drifter_time, argo_time)
+    postprocess()
