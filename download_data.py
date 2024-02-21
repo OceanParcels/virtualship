c33ece192f63c374a0ccf2e64201f0fe01a84a78
Emma Daniels <e.e.daniels1@uu.nl>
2024-02-21 17:08:01 +0100

    added download_data script

--- download_data.py
+++ download_data.py
@@ -1,6 +1,7 @@
 import os
 import json
-# import copernicusmarine as cm
+import copernicusmarine
+import datetime
 
 
 class VirtualShipConfiguration:
@@ -11,31 +12,72 @@ class VirtualShipConfiguration:
                 setattr(self, key, json_input[key])
 
 
-
-def create_json(config):
-
-    data_dict = {
-    "dataset_id": "cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i",
-    "variables": ["uo", "vo"],
-    "minimum_longitude": config.region_of_interest["West"],
-    "maximum_longitude": config.region_of_interest["East"],
-    "minimum_latitude": config.region_of_interest["South"],
-    "maximum_latitude": config.region_of_interest["North"],
-    "start_datetime": config.requested_ship_time["start"],
-    "end_datetime": config.requested_ship_time["end"],
-    "minimum_depth": 0.49402499198913574,
-    "maximum_depth": 5727.9169921875,
-    "output_filename":  "CMEMS_Indian_currents_Jan2022.nc",
-    "output_directory":  "group_A"
-    }
-
-    with open("sample.json", "w") as outfile:
-        json.dump(data_dict, outfile)
-
-
+download_dict = {
+    'UVdata': {'dataset_id': 'cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i', 'variables': ['uo', 'vo'], 'output_filename': "studentdata_UV.nc",},
+    'Sdata': {'dataset_id': 'cmems_mod_glo_phy-so_anfc_0.083deg_PT6H-i', 'variables': ['so'], 'output_filename': "studentdata_S.nc"},
+    'Tdata': {'dataset_id': 'cmems_mod_glo_phy-thetao_anfc_0.083deg_PT6H-i', 'variables': ['thetao'], 'output_filename': "studentdata_T.nc"}
+}
 
 
 if __name__ == '__main__':
     config = VirtualShipConfiguration('student_input.json')
-    create_json(config)
-    copernicusmarine.subset(request_file = "sample.json")
+
+    for dataset in download_dict:
+        copernicusmarine.subset(
+            dataset_id=download_dict[dataset]['dataset_id'],
+            variables=download_dict[dataset]['variables'],
+            minimum_longitude=config.region_of_interest["West"],
+            maximum_longitude=config.region_of_interest["East"],
+            minimum_latitude=config.region_of_interest["South"],
+            maximum_latitude=config.region_of_interest["North"],
+            start_datetime=config.requested_ship_time["start"],
+            end_datetime=config.requested_ship_time["end"],
+            minimum_depth=0.49402499198913574,
+            maximum_depth=5727.9169921875,
+            output_filename=download_dict[dataset]['output_filename']
+        )
+
+
+    if len(config.drifter_deploylocations) > 0:
+
+        download_dict = {
+            'UVdata': {'dataset_id': 'cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i', 'variables': ['uo', 'vo'], 'output_filename': "drifterdata_UV.nc",},
+            'Tdata': {'dataset_id': 'cmems_mod_glo_phy-thetao_anfc_0.083deg_PT6H-i', 'variables': ['thetao'], 'output_filename': "drifterdata_T.nc"}
+        }    
+
+        for dataset in download_dict:
+            copernicusmarine.subset(
+                dataset_id=download_dict[dataset]['dataset_id'],
+                variables=download_dict[dataset]['variables'],
+                minimum_longitude=config.region_of_interest["West"]-5,
+                maximum_longitude=config.region_of_interest["East"]+5,
+                minimum_latitude=config.region_of_interest["South"]-5,
+                maximum_latitude=config.region_of_interest["North"]+5,
+                start_datetime=config.requested_ship_time["start"],
+                end_datetime=f'{datetime.datetime.strptime(config.requested_ship_time["end"],"%Y-%m-%dT%H:%M:%S")+datetime.timedelta(days=21):%Y-%m-%dT%H:%M:%S}',
+                minimum_depth=0.49402499198913574,
+                maximum_depth=0.49402499198913574,
+                output_filename=download_dict[dataset]['output_filename']
+            )
+
+    if len(config.argo_deploylocations) > 0:
+        download_dict = {
+            'UVdata': {'dataset_id': 'cmems_mod_glo_phy-cur_anfc_0.083deg_PT6H-i', 'variables': ['uo', 'vo'], 'output_filename': "argosdata_UV.nc",},
+            'Sdata' : {'dataset_id': 'cmems_mod_glo_phy-so_anfc_0.083deg_PT6H-i', 'variables': ['so'], 'output_filename': "argosdata_S.nc"},
+            'Tdata': {'dataset_id': 'cmems_mod_glo_phy-thetao_anfc_0.083deg_PT6H-i', 'variables': ['thetao'], 'output_filename': "argosdata_T.nc"}
+        }    
+
+        for dataset in download_dict:
+            copernicusmarine.subset(
+                dataset_id=download_dict[dataset]['dataset_id'],
+                variables=download_dict[dataset]['variables'],
+                minimum_longitude=config.region_of_interest["West"]-5,
+                maximum_longitude=config.region_of_interest["East"]+5,
+                minimum_latitude=config.region_of_interest["South"]-5,
+                maximum_latitude=config.region_of_interest["North"]+5,
+                start_datetime=config.requested_ship_time["start"],
+                end_datetime=f'{datetime.datetime.strptime(config.requested_ship_time["end"],"%Y-%m-%dT%H:%M:%S")+datetime.timedelta(days=21):%Y-%m-%dT%H:%M:%S}',
+                minimum_depth=0.49402499198913574,
+                maximum_depth=5727.9169921875,
+                output_filename=download_dict[dataset]['output_filename']
+            )
