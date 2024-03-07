ad0bca16d9dc924fcdc7a5efdbae574ccc7bebb7
Emma Daniels <e.e.daniels1@uu.nl>
2024-03-07 11:42:47 +0100

    not run postprocess if no CTDs

--- virtualship.py
+++ virtualship.py
@@ -532,48 +532,49 @@ def argo_deployments(config, argo_time):
 def postprocess():
     '''Postprocesses CTD data and writes to csv files'''
 
-    i = 0
-    for filename in os.scandir(os.path.join("results","CTDs")):
-        # not so happy about using filename and i as they might not be identical
-        if filename.path.endswith(".zarr"):
-            try: 
-                i += 1
-                # Open output and read to x, y, z
-                ds = xr.open_zarr(filename.path)
-                x = ds["lon"][:].squeeze()
-                y = ds["lat"][:].squeeze()
-                z = ds["z"][:].squeeze()
-                time = ds["time"][:].squeeze()
-                T = ds["temperature"][:].squeeze()
-                S = ds["salinity"][:].squeeze()
-                ds.close()
+    if os.path.isdir(os.path.join("results","CTDs")):
+        i = 0
+        for filename in os.scandir(os.path.join("results","CTDs")):
+            # not so happy about using filename and i as they might not be identical
+            if filename.path.endswith(".zarr"):
+                try: 
+                    i += 1
+                    # Open output and read to x, y, z
+                    ds = xr.open_zarr(filename.path)
+                    x = ds["lon"][:].squeeze()
+                    y = ds["lat"][:].squeeze()
+                    z = ds["z"][:].squeeze()
+                    time = ds["time"][:].squeeze()
+                    T = ds["temperature"][:].squeeze()
+                    S = ds["salinity"][:].squeeze()
+                    ds.close()
 
-                if (time is not None) and (time.size!=0):
-                    random_walk = np.random.random()/10
-                    z_norm = (z-np.min(z))/(np.max(z)-np.min(z))
-                    t_norm = np.linspace(0, 1, num=len(time))
-                    # add smoothed random noise scaled with depth
-                    # and random (reversed) diversion from initial through time scaled with depth
-                    S = S + uniform_filter1d(
-                        np.random.random(S.shape)/5*(1-z_norm) +
-                        random_walk*(np.max(S).values - np.min(S).values)*(1-z_norm)*t_norm/10,
-                        max(int(len(time)/40), 1))
-                    T = T + uniform_filter1d(
-                        np.random.random(T.shape)*5*(1-z_norm) -
-                        random_walk/2*(np.max(T).values - np.min(T).values)*(1-z_norm)*t_norm/10,
-                        max(int(len(time)/20), 1))
+                    if (time is not None) and (time.size!=0):
+                        random_walk = np.random.random()/10
+                        z_norm = (z-np.min(z))/(np.max(z)-np.min(z))
+                        t_norm = np.linspace(0, 1, num=len(time))
+                        # add smoothed random noise scaled with depth
+                        # and random (reversed) diversion from initial through time scaled with depth
+                        S = S + uniform_filter1d(
+                            np.random.random(S.shape)/5*(1-z_norm) +
+                            random_walk*(np.max(S).values - np.min(S).values)*(1-z_norm)*t_norm/10,
+                            max(int(len(time)/40), 1))
+                        T = T + uniform_filter1d(
+                            np.random.random(T.shape)*5*(1-z_norm) -
+                            random_walk/2*(np.max(T).values - np.min(T).values)*(1-z_norm)*t_norm/10,
+                            max(int(len(time)/20), 1))
 
-                # reshaping data to export to csv
-                header = f"pressure [dbar],temperature [degC],salinity [g kg-1]"
-                data = np.column_stack([-z, T, S])
-                new_line = '\n'
-                np.savetxt(f"{os.path.join('results','CTDs','CTD_station_')}{i}.csv", data, fmt="%.4f", header=header, delimiter=',',
-                            comments=f'longitude,{x[0].values},"{x.attrs}"{new_line}latitude,{y[0].values},"{y.attrs}"{new_line}start time,{time[0].values}{new_line}end time,{time[-1].values}{new_line}')
-                shutil.rmtree(filename.path)
-            except TypeError:
-                print(f"CTD file {filename} seems faulty, skipping.")
-                continue
-    print("CTD data postprocessed.")
+                    # reshaping data to export to csv
+                    header = f"pressure [dbar],temperature [degC],salinity [g kg-1]"
+                    data = np.column_stack([-z, T, S])
+                    new_line = '\n'
+                    np.savetxt(f"{os.path.join('results','CTDs','CTD_station_')}{i}.csv", data, fmt="%.4f", header=header, delimiter=',',
+                                comments=f'longitude,{x[0].values},"{x.attrs}"{new_line}latitude,{y[0].values},"{y.attrs}"{new_line}start time,{time[0].values}{new_line}end time,{time[-1].values}{new_line}')
+                    shutil.rmtree(filename.path)
+                except TypeError:
+                    print(f"CTD file {filename} seems faulty, skipping.")
+                    continue
+        print("CTD data postprocessed.")
 
 if __name__ == '__main__':
     config = VirtualShipConfiguration('student_input.json')
