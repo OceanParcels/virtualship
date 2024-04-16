aa51e18480d1cf9f090f8a31eb1100f1a7eed2ee
Aart Stuurman <aartstuurman@hotmail.com>
2024-04-16 15:15:24 +0200

    Add missing import

--- virtual_ship/virtual_ship_configuration.py
+++ virtual_ship/virtual_ship_configuration.py
@@ -2,10 +2,12 @@ from shapely.geometry import Point, Polygon
 import os
 import json
 import datetime
+from datetime import timedelta
+
 
 class VirtualShipConfiguration:
     def __init__(self, json_file):
-        with open(os.path.join(os.path.dirname(__file__), json_file), 'r') as file:
+        with open(os.path.join(os.path.dirname(__file__), json_file), "r") as file:
             json_input = json.loads(file.read())
             for key in json_input:
                 setattr(self, key, json_input[key])
@@ -17,16 +19,31 @@ class VirtualShipConfiguration:
         west = self.region_of_interest["West"]
         poly = Polygon([(west, north), (west, south), (east, south), (east, north)])
         # validate input, raise ValueErrors if invalid
-        if self.region_of_interest["North"] > 90 or self.region_of_interest["South"] < -90 or self.region_of_interest["East"] > 180 or self.region_of_interest["West"] < -180:
+        if (
+            self.region_of_interest["North"] > 90
+            or self.region_of_interest["South"] < -90
+            or self.region_of_interest["East"] > 180
+            or self.region_of_interest["West"] < -180
+        ):
             raise ValueError("Invalid coordinates in region of interest")
         if self.requested_ship_time["start"] > self.requested_ship_time["end"]:
             raise ValueError("Start time should be before end time")
-        if datetime.datetime.strptime(self.requested_ship_time["end"],"%Y-%m-%dT%H:%M:%S") > datetime.datetime.now()+timedelta(days=2):
+        if datetime.datetime.strptime(
+            self.requested_ship_time["end"], "%Y-%m-%dT%H:%M:%S"
+        ) > datetime.datetime.now() + timedelta(days=2):
             raise ValueError("End time cannot be more then 2 days into the future")
-        if datetime.datetime.strptime(self.requested_ship_time["end"],"%Y-%m-%dT%H:%M:%S") - datetime.datetime.strptime(self.requested_ship_time["start"],"%Y-%m-%dT%H:%M:%S") > timedelta(days=21):
+        if datetime.datetime.strptime(
+            self.requested_ship_time["end"], "%Y-%m-%dT%H:%M:%S"
+        ) - datetime.datetime.strptime(
+            self.requested_ship_time["start"], "%Y-%m-%dT%H:%M:%S"
+        ) > timedelta(
+            days=21
+        ):
             raise ValueError("The time period is too long, maximum is 21 days")
         if len(self.route_coordinates) < 2:
-            raise ValueError("Route needs to consist of at least 2 longitude-latitude coordinate sets")
+            raise ValueError(
+                "Route needs to consist of at least 2 longitude-latitude coordinate sets"
+            )
         for coord in self.route_coordinates:
             if coord[1] > 90 or coord[1] < -90 or coord[0] > 180 or coord[0] < -180:
                 raise ValueError("Invalid coordinates in route")
@@ -39,12 +56,16 @@ class VirtualShipConfiguration:
                 if coord[1] > 90 or coord[1] < -90 or coord[0] > 180 or coord[0] < -180:
                     raise ValueError("Invalid coordinates in route")
                 if not poly.contains(Point(coord)):
-                    raise ValueError("CTD coordinates need to be within the region of interest")
-        if type(self.CTD_settings['max_depth']) != int:
-            if self.CTD_settings['max_depth'] != "max":
-                raise ValueError('Specify "max" for maximum depth or a negative integer for max_depth in CTD_settings')
-        if type(self.CTD_settings['max_depth']) == int:
-            if self.CTD_settings['max_depth'] > 0:
+                    raise ValueError(
+                        "CTD coordinates need to be within the region of interest"
+                    )
+        if type(self.CTD_settings["max_depth"]) != int:
+            if self.CTD_settings["max_depth"] != "max":
+                raise ValueError(
+                    'Specify "max" for maximum depth or a negative integer for max_depth in CTD_settings'
+                )
+        if type(self.CTD_settings["max_depth"]) == int:
+            if self.CTD_settings["max_depth"] > 0:
                 raise ValueError("Invalid depth for CTD")
         if len(self.drifter_deploylocations) > 30:
             raise ValueError("Too many drifter deployment locations, maximum is 30")
@@ -55,7 +76,9 @@ class VirtualShipConfiguration:
                 if coord[1] > 90 or coord[1] < -90 or coord[0] > 180 or coord[0] < -180:
                     raise ValueError("Invalid coordinates in route")
                 if not poly.contains(Point(coord)):
-                    raise ValueError("Drifter coordinates need to be within the region of interest")
+                    raise ValueError(
+                        "Drifter coordinates need to be within the region of interest"
+                    )
         if len(self.argo_deploylocations) > 30:
             raise ValueError("Too many argo deployment locations, maximum is 30")
         if not len(self.argo_deploylocations) == 0:
@@ -65,26 +88,48 @@ class VirtualShipConfiguration:
                 if coord[1] > 90 or coord[1] < -90 or coord[0] > 180 or coord[0] < -180:
                     raise ValueError("Invalid coordinates in route")
                 if not poly.contains(Point(coord)):
-                    raise ValueError("argo coordinates need to be within the region of interest")
+                    raise ValueError(
+                        "argo coordinates need to be within the region of interest"
+                    )
         if not isinstance(self.underway_data, bool):
             raise ValueError("Underway data needs to be true or false")
         if not isinstance(self.ADCP_data, bool):
             raise ValueError("ADCP data needs to be true or false")
-        if self.ADCP_settings['bin_size_m'] < 0 or self.ADCP_settings['bin_size_m'] > 24:
+        if (
+            self.ADCP_settings["bin_size_m"] < 0
+            or self.ADCP_settings["bin_size_m"] > 24
+        ):
             raise ValueError("Invalid bin size for ADCP")
-        if self.ADCP_settings['max_depth'] > 0:
+        if self.ADCP_settings["max_depth"] > 0:
             raise ValueError("Invalid depth for ADCP")
-        if self.argo_characteristics['driftdepth'] > 0 or self.argo_characteristics['driftdepth'] < -5727:
-            raise ValueError("Specify negative depth. Max drift depth for argo is -5727 m due to data availability")
-        if self.argo_characteristics['maxdepth'] > 0 or self.argo_characteristics['maxdepth'] < -5727:
-            raise ValueError("Specify negative depth. Max depth for argo is -5727 m due to data availability")
-        if type(self.argo_characteristics['vertical_speed']) != float:
+        if (
+            self.argo_characteristics["driftdepth"] > 0
+            or self.argo_characteristics["driftdepth"] < -5727
+        ):
+            raise ValueError(
+                "Specify negative depth. Max drift depth for argo is -5727 m due to data availability"
+            )
+        if (
+            self.argo_characteristics["maxdepth"] > 0
+            or self.argo_characteristics["maxdepth"] < -5727
+        ):
+            raise ValueError(
+                "Specify negative depth. Max depth for argo is -5727 m due to data availability"
+            )
+        if type(self.argo_characteristics["vertical_speed"]) != float:
             raise ValueError("Specify vertical speed for argo with decimals in m/s")
-        if self.argo_characteristics['vertical_speed'] > 0:
-            self.argo_characteristics['vertical_speed'] = -self.argo_characteristics['vertical_speed']
-        if abs(self.argo_characteristics['vertical_speed']) < 0.06 or abs(self.argo_characteristics['vertical_speed']) > 0.12:
-            raise ValueError("Specify a realistic speed for argo, i.e. between -0.06 and -0.12 m/s")
-        if self.argo_characteristics['cycle_days'] < 0:
+        if self.argo_characteristics["vertical_speed"] > 0:
+            self.argo_characteristics["vertical_speed"] = -self.argo_characteristics[
+                "vertical_speed"
+            ]
+        if (
+            abs(self.argo_characteristics["vertical_speed"]) < 0.06
+            or abs(self.argo_characteristics["vertical_speed"]) > 0.12
+        ):
+            raise ValueError(
+                "Specify a realistic speed for argo, i.e. between -0.06 and -0.12 m/s"
+            )
+        if self.argo_characteristics["cycle_days"] < 0:
             raise ValueError("Specify a postitive number of cycle days for argo")
-        if self.argo_characteristics['drift_days'] < 0:
+        if self.argo_characteristics["drift_days"] < 0:
             raise ValueError("Specify a postitive number of drift days for argo")
