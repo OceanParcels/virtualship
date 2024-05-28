3524ab327d078fc1548e899e50bbee7405f1e76b
Aart Stuurman <aartstuurman@hotmail.com>
2024-05-28 16:44:45 +0200

    drifters instrument. just missing test

--- virtual_ship/instruments/__init__.py
+++ virtual_ship/instruments/__init__.py
@@ -1,6 +1,6 @@
 """Measurement instrument that can be used with Parcels."""
 
-from . import argo_float
+from . import argo_float, drifter
 from .location import Location
 
-__all__ = ["Location", "argo_float"]
+__all__ = ["Location", "argo_float", "drifter"]
--- virtual_ship/instruments/drifter.py
+++ virtual_ship/instruments/drifter.py
@@ -1,35 +1,37 @@
-"""Argo float instrument."""
+"""Drifter instrument."""
 
-import math
 from dataclasses import dataclass
 from datetime import timedelta
 
 import numpy as np
-from parcels import (
-    AdvectionRK4,
-    FieldSet,
-    JITParticle,
-    ParticleSet,
-    StatusCode,
-    Variable,
-)
+from parcels import AdvectionRK4, FieldSet, JITParticle, ParticleSet, Variable
 
 from .location import Location
 
 
 @dataclass
 class Drifter:
-    """Configuration for a single Argo float."""
+    """Configuration for a single Drifter."""
 
     location: Location
     deployment_time: float
+    min_depth: float
 
 
-class _ArgoParticle(JITParticle):
+class _DrifterParticle(JITParticle):
     temperature = Variable(
         "temperature",
         initial=np.nan,
     )
+
+
+def _sample_temperature(particle, fieldset, time):
+    particle.temperature = fieldset.T[time, particle.depth, particle.lat, particle.lon]
+
+
+def _check_error(particle, fieldset, time):
+    if particle.state >= 50:  # This captures all Errors
+        particle.delete()
 
 
 def simulate_drifters(
@@ -39,35 +41,29 @@ def simulate_drifters(
     outputdt: timedelta,
 ) -> None:
     """
-    Use parcels to simulate a set of Argo floats in a fieldset.
+    Use parcels to simulate a set of drifters in a fieldset.
 
-    :param argo_floats: A list of Argo floats to simulate.
-    :param fieldset: The fieldset to simulate the Argo floats in.
+    :param drifters: A list of drifters to simulate.
+    :param fieldset: The fieldset to simulate the drifters in.
     :param out_file_name: The file to write the results to.
     :param outputdt: Interval which dictates the update frequency of file output during simulation
     """
-    lon = [argo.location.lon for argo in argo_floats]
-    lat = [argo.location.lat for argo in argo_floats]
-    time = [argo.deployment_time for argo in argo_floats]
+    lon = [drifter.location.lon for drifter in drifters]
+    lat = [drifter.location.lat for drifter in drifters]
+    time = [drifter.deployment_time for drifter in drifters]
 
     # define the parcels simulation
-    argo_float_fieldset = ParticleSet(
+    drifter_particleset = ParticleSet(
         fieldset=fieldset,
-        pclass=_ArgoParticle,
+        pclass=_DrifterParticle,
         lon=lon,
         lat=lat,
-        depth=[argo.min_depth for argo in argo_floats],
+        depth=[drifter.min_depth for drifter in drifters],
         time=time,
-        min_depth=[argo.min_depth for argo in argo_floats],
-        max_depth=[argo.max_depth for argo in argo_floats],
-        drift_depth=[argo.drift_depth for argo in argo_floats],
-        vertical_speed=[argo.vertical_speed for argo in argo_floats],
-        cycle_days=[argo.cycle_days for argo in argo_floats],
-        drift_days=[argo.drift_days for argo in argo_floats],
     )
 
     # define output file for the simulation
-    out_file = argo_float_fieldset.ParticleFile(
+    out_file = drifter_particleset.ParticleFile(
         name=out_file_name,
         outputdt=timedelta(minutes=5),
         chunks=(1, 500),
@@ -77,7 +73,7 @@ def simulate_drifters(
     fieldset_endtime = fieldset.time_origin.fulltime(fieldset.U.grid.time_full[-1])
 
     # execute simulation
-    drifter_fieldset.execute(
+    drifter_particleset.execute(
         [AdvectionRK4, _sample_temperature, _check_error],
         endtime=fieldset_endtime,
         dt=outputdt,
