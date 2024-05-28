1e1267f52fe8cd7c2de3ad6ebbe6550c40285b7e
Aart Stuurman <aartstuurman@hotmail.com>
2024-05-28 14:43:04 +0200

    Create proper init.py

--- virtual_ship/__init__.py
+++ virtual_ship/__init__.py
@@ -1,1 +1,6 @@
 """Code for the Virtual Ship Classroom, where Marine Scientists can combine Copernicus Marine Data with an OceanParcels ship to go on a virtual expedition."""
+
+import instruments
+import sailship
+
+__all__ = ["instruments", "sailship"]
