9c00fcf334e0a70ebc8c45695066e88a4f957ac3
Aart Stuurman <aartstuurman@hotmail.com>
2024-05-28 17:03:34 +0200

    drifter test

---
+++ tests/instruments/test_drifters.py
@@ -0,0 +1,35 @@
+"""Test the simulation of drifters."""
+
+from virtual_ship.instruments.drifter import simulate_drifters, Drifter
+from virtual_ship.instruments import Location
+from parcels import FieldSet
+import numpy as np
+from datetime import timedelta
+
+
+def test_simulate_drifters() -> None:
+    fieldset = FieldSet.from_data(
+        {"U": 0, "V": 0, "T": 0},
+        {
+            "lon": 0,
+            "lat": 0,
+            "time": [np.datetime64("1950-01-01") + np.timedelta64(632160, "h")],
+        },
+    )
+
+    min_depth = -fieldset.U.depth[0]
+
+    drifters = [
+        Drifter(
+            location=Location(latitude=0, longitude=0),
+            deployment_time=0,
+            min_depth=min_depth,
+        )
+    ]
+
+    simulate_drifters(
+        drifters=drifters,
+        fieldset=fieldset,
+        out_file_name="test",
+        outputdt=timedelta(minutes=5),
+    )
