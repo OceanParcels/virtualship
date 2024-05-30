ae45956b64fbb2824415b2a7792ce13c68956341
Aart Stuurman <aartstuurman@hotmail.com>
2024-05-30 13:11:39 +0200

    Cleanup old files or move to lesson_material directory (#34)

--- virtualship.py
+++
@@ -1,6 +0,0 @@
-from virtual_ship.virtual_ship_configuration import VirtualShipConfiguration
-from virtual_ship.sailship import sailship
-
-if __name__ == "__main__":
-    config = VirtualShipConfiguration("student_input.json")
-    sailship(config)
