98b17c7291f53f417802b80dbaf5005a0160b58a
Aart Stuurman <aartstuurman@hotmail.com>
2024-06-02 12:47:37 +0200

    first try adcp

---
+++ virtual_ship/instruments/adcp.py
@@ -0,0 +1,65 @@
+"""ADCP."""
+
+from dataclasses import dataclass
+from datetime import timedelta
+
+import numpy as np
+from parcels import FieldSet, JITParticle, ParticleSet, Variable
+
+from .location import Location
+
+
+@dataclass
+class SamplePoint:
+    location: Location
+    time: float
+
+
+class _ADCPParticle(JITParticle):
+    """ADCP particle sampling a single point at some depth."""
+
+    U = Variable("U", dtype=np.float32, initial=0.0)
+    V = Variable("V", dtype=np.float32, initial=0.0)
+
+
+def _sample_velocity(particle, fieldset, time):
+    particle.U, particle.V = fieldset.UV.eval(
+        time, particle.depth, particle.lat, particle.lon, applyConversion=False
+    )
+
+
+def simulate_adcp(
+    fieldset: FieldSet,
+    out_file_name: str,
+    max_depth: float,
+    min_depth: float,
+    bin_size: float,
+    sample_points: list[SamplePoint],
+) -> None:
+    sample_points.sort(key=lambda p: p.time)
+
+    bins = np.arange(max_depth, min_depth, bin_size)
+    num_particles = len(bins)
+    particleset = ParticleSet.from_list(
+        fieldset=fieldset,
+        pclass=_ADCPParticle,
+        lon=np.full(
+            num_particles, 0.0
+        ),  # initial lat/lon are irrelevant and will be overruled later.
+        lat=np.full(num_particles, 0.0),
+        depth=bins,
+        time=0,  # same for time
+    )
+
+    # define output file for the simulation
+    out_file = particleset.ParticleFile(
+        name=out_file_name,
+    )
+
+    for point in sample_points:
+        particleset.lon_nextloop[:] = point.location.lon
+        particleset.lat_nextloop[:] = point.location.lat
+        particleset.time_nextloop[:] = point.time
+
+        particleset.execute([_sample_velocity], dt=1, runtime=1, verbose_progress=False)
+        out_file.write(particleset, time=particleset[0].time)
