f92839d04929958a7e0c1126606b089d5b45ea0e
Aart Stuurman <aartstuurman@hotmail.com>
2024-06-24 11:18:09 +0200

    ADCP test (#37)
    * ADCP instrument cleanup and test
    * Ship ST instrument test improvements

--- tests/instruments/test_ship_underwater_st.py
+++ tests/instruments/test_ship_underwater_st.py
@@ -18,50 +18,50 @@ def test_simulate_ship_underwater_st(tmpdir: py.path.LocalPath) -> None:
     # arbitrary time offset for the dummy fieldset
     base_time = datetime.datetime.strptime("1950-01-01", "%Y-%m-%d")
 
-    # variabes we are going to compare between expected and actual observations
-    variables = ["salinity", "temperature", "lat", "lon"]
-
     # where to sample
     sample_points = [
-        Spacetime(Location(3, 0), base_time + datetime.timedelta(seconds=0)),
-        Spacetime(Location(7, 0), base_time + datetime.timedelta(seconds=1)),
+        Spacetime(Location(1, 2), base_time + datetime.timedelta(seconds=0)),
+        Spacetime(Location(3, 4), base_time + datetime.timedelta(seconds=1)),
     ]
 
     # expected observations at sample points
     expected_obs = [
         {
-            "salinity": 1,
-            "temperature": 2,
-            "lat": 3,
-            "lon": 0,
+            "salinity": 5,
+            "temperature": 6,
+            "lat": sample_points[0].location.lat,
+            "lon": sample_points[0].location.lon,
             "time": base_time + datetime.timedelta(seconds=0),
         },
         {
-            "salinity": 5,
-            "temperature": 6,
-            "lat": 7,
-            "lon": 0,
+            "salinity": 7,
+            "temperature": 8,
+            "lat": sample_points[1].location.lat,
+            "lon": sample_points[1].location.lon,
             "time": base_time + datetime.timedelta(seconds=1),
         },
     ]
 
     # create fieldset based on the expected observations
+    # indices are time, latitude, longitude
+    salinity = np.zeros((2, 2, 2))
+    salinity[0, 0, 0] = expected_obs[0]["salinity"]
+    salinity[1, 1, 1] = expected_obs[1]["salinity"]
+
+    temperature = np.zeros((2, 2, 2))
+    temperature[0, 0, 0] = expected_obs[0]["temperature"]
+    temperature[1, 1, 1] = expected_obs[1]["temperature"]
+
     fieldset = FieldSet.from_data(
         {
-            "U": np.zeros((2, 2)),
-            "V": np.zeros((2, 2)),
-            "salinity": [
-                [expected_obs[0]["salinity"], 0],
-                [0, expected_obs[1]["salinity"]],
-            ],
-            "temperature": [
-                [expected_obs[0]["temperature"], 0],
-                [0, expected_obs[1]["temperature"]],
-            ],
+            "V": np.zeros((2, 2, 2)),
+            "U": np.zeros((2, 2, 2)),
+            "salinity": salinity,
+            "temperature": temperature,
         },
         {
-            "lon": 0,
             "lat": np.array([expected_obs[0]["lat"], expected_obs[1]["lat"]]),
+            "lon": np.array([expected_obs[0]["lon"], expected_obs[1]["lon"]]),
             "time": np.array(
                 [
                     np.datetime64(expected_obs[0]["time"]),
@@ -71,6 +71,7 @@ def test_simulate_ship_underwater_st(tmpdir: py.path.LocalPath) -> None:
         },
     )
 
+    # perform simulation
     out_path = tmpdir.join("out.zarr")
 
     simulate_ship_underwater_st(
@@ -80,21 +81,23 @@ def test_simulate_ship_underwater_st(tmpdir: py.path.LocalPath) -> None:
         sample_points=sample_points,
     )
 
+    # test if output is as expected
     results = xr.open_zarr(out_path)
 
-    # below we assert if output makes sense
-    assert len(results.trajectory) == 1  # expect a singe trajectory
+    assert len(results.trajectory) == 1  # expect a single trajectory
     traj = results.trajectory.item()
     assert len(results.sel(trajectory=traj).obs) == len(
         sample_points
     )  # expect as many obs as sample points
 
     # for every obs, check if the variables match the expected observations
-    for obs_i, exp in zip(results.sel(trajectory=traj).obs, expected_obs, strict=True):
+    for i, (obs_i, exp) in enumerate(
+        zip(results.sel(trajectory=traj).obs, expected_obs, strict=True)
+    ):
         obs = results.sel(trajectory=traj, obs=obs_i)
-        for var in variables:
+        for var in ["salinity", "temperature", "lat", "lon"]:
             obs_value = obs[var].values.item()
             exp_value = exp[var]
             assert np.isclose(
                 obs_value, exp_value
-            ), f"Observation incorrect {obs_i=} {var=} {obs_value=} {exp_value=}."
+            ), f"Observation incorrect {i=} {var=} {obs_value=} {exp_value=}."
