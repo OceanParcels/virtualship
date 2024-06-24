987a8a7f45b05c6ee9a6a85e5fdf3c390ad8cf69
Aart Stuurman <aartstuurman@hotmail.com>
2024-06-24 16:57:25 +0200

    wip

--- tests/instruments/test_ctd.py
+++ tests/instruments/test_ctd.py
@@ -4,32 +4,47 @@ from datetime import timedelta
 
 import numpy as np
 import py
-from parcels import FieldSet
+from parcels import FieldSet, Field
 
 from virtual_ship import Location, Spacetime
 from virtual_ship.instruments.ctd import CTD, simulate_ctd
+import datetime
+import xarray as xr
 
 
 def test_simulate_ctds(tmpdir: py.path.LocalPath) -> None:
-    fieldset = FieldSet.from_data(
-        {"U": 0, "V": 0, "T": 0, "S": 0, "bathymetry": 100},
-        {
-            "lon": 0,
-            "lat": 0,
-            "time": [np.datetime64("1950-01-01") + np.timedelta64(632160, "h")],
-        },
-    )
-
-    min_depth = -fieldset.U.depth[0]
-    max_depth = -fieldset.U.depth[-1]
+    # arbitrary time offset for the dummy fieldset
+    base_time = datetime.datetime.strptime("1950-01-01", "%Y-%m-%d")
 
     ctds = [
         CTD(
-            spacetime=Spacetime(location=Location(latitude=0, longitude=0), time=0),
-            min_depth=min_depth,
-            max_depth=max_depth,
+            spacetime=Spacetime(
+                location=Location(latitude=0, longitude=0),
+                time=base_time + datetime.timedelta(seconds=0),
+            ),
+            min_depth=0,
+            max_depth=-500,
         )
     ]
+
+    u = np.zeros((2, 2, 2, 2))
+    v = np.zeros((2, 2, 2, 2))
+    t = np.zeros((2, 2, 2, 2))
+    s = np.zeros((2, 2, 2, 2))
+
+    fieldset = FieldSet.from_data(
+        {"V": v, "U": u, "T": t, "S": s},
+        {
+            "time": [
+                np.datetime64(base_time + datetime.timedelta(seconds=0)),
+                np.datetime64(base_time + datetime.timedelta(minutes=30)),
+            ],
+            "depth": [0, -1000],
+            "lat": [0, 1],
+            "lon": [0, 1],
+        },
+    )
+    fieldset.add_field(Field("bathymetry", [-1000], lon=0, lat=0))
 
     out_path = tmpdir.join("out.zarr")
 
@@ -39,3 +54,25 @@ def test_simulate_ctds(tmpdir: py.path.LocalPath) -> None:
         out_path=out_path,
         outputdt=timedelta(seconds=10),
     )
+
+    # test if output is as expected
+    results = xr.open_zarr(out_path)
+    e = 3
+
+    # assert len(results.trajectory) == 1  # expect a single trajectory
+    # traj = results.trajectory.item()
+    # assert len(results.sel(trajectory=traj).obs) == len(
+    #     sample_points
+    # )  # expect as many obs as sample points
+
+    # # for every obs, check if the variables match the expected observations
+    # for i, (obs_i, exp) in enumerate(
+    #     zip(results.sel(trajectory=traj).obs, expected_obs, strict=True)
+    # ):
+    #     obs = results.sel(trajectory=traj, obs=obs_i)
+    #     for var in ["salinity", "temperature", "lat", "lon"]:
+    #         obs_value = obs[var].values.item()
+    #         exp_value = exp[var]
+    #         assert np.isclose(
+    #             obs_value, exp_value
+    #         ), f"Observation incorrect {i=} {var=} {obs_value=} {exp_value=}."
--- virtual_ship/instruments/ctd.py
+++ virtual_ship/instruments/ctd.py
@@ -5,7 +5,14 @@ from datetime import timedelta
 
 import numpy as np
 import py
-from parcels import FieldSet, JITParticle, ParticleSet, Variable
+from parcels import (
+    FieldSet,
+    ScipyParticle,
+    JITParticle,
+    ParticleSet,
+    Variable,
+    StatusCode,
+)
 
 from ..spacetime import Spacetime
 
@@ -19,12 +26,13 @@ class CTD:
     max_depth: float
 
 
-_CTDParticle = JITParticle.add_variables(
+_CTDParticle = ScipyParticle.add_variables(
     [
         Variable("salinity", dtype=np.float32, initial=np.nan),
         Variable("temperature", dtype=np.float32, initial=np.nan),
         Variable("raising", dtype=np.int32, initial=0),
         Variable("max_depth", dtype=np.float32),
+        Variable("winch_speed", dtype=np.float32),
     ]
 )
 
@@ -38,32 +46,19 @@ def _sample_salinity(particle, fieldset, time):
 
 
 def _ctd_cast(particle, fieldset, time):
-    # Lowering and raising CTD
-    if (
-        -fieldset.bathymetry[time, particle.depth, particle.lat, particle.lon]
-        > particle.max_depth
-    ):
-        maxdepth = (
-            -fieldset.bathymetry[time, particle.depth, particle.lat, particle.lon] + 20
-        )
-    else:
-        maxdepth = particle.max_depth
-    winch_speed = -1.0  # sink and rise speed in m/s
-
     if particle.raising == 0:
         # Sinking with winch_speed until near seafloor
-        particle_ddepth = winch_speed * particle.dt
-        if particle.depth <= maxdepth:
+        particle_ddepth = -particle.winch_speed * particle.dt
+        if particle.depth <= particle.max_depth:
             particle.raising = 1
 
     if particle.raising == 1:
         # Rising with winch_speed until depth is -2 m
         if particle.depth < -2:
-            particle_ddepth = -winch_speed * particle.dt
+            particle_ddepth = particle.winch_speed * particle.dt
             if particle.depth + particle_ddepth >= -2:
                 # to break the loop ...
-                particle.state = 41
-                print("CTD cast finished.")
+                particle.state = StatusCode.StopAllExecution
 
 
 def simulate_ctd(
@@ -80,35 +75,50 @@ def simulate_ctd(
     :param ctds: A list of CTDs to simulate.
     :param outputdt: Interval which dictates the update frequency of file output during simulation
     """
-    lon = [ctd.spacetime.location.lon for ctd in ctds]
-    lat = [ctd.spacetime.location.lat for ctd in ctds]
-    time = [ctd.spacetime.time for ctd in ctds]
+    WINCH_SPEED = 1.0  # sink and rise speed in m/s
 
-    # define parcel particles
-    ctd_particleset = ParticleSet(
-        fieldset=fieldset,
-        pclass=_CTDParticle,
-        lon=lon,
-        lat=lat,
-        depth=[ctd.min_depth for ctd in ctds],
-        time=time,
-        max_depth=[ctd.max_depth for ctd in ctds],
-    )
-
-    # define output file for the simulation
-    out_file = ctd_particleset.ParticleFile(
-        name=out_path,
-        outputdt=outputdt,
-        chunks=(1, 500),
-    )
-
-    # get time when the fieldset ends
+    fieldset_starttime = fieldset.time_origin.fulltime(fieldset.U.grid.time_full[0])
     fieldset_endtime = fieldset.time_origin.fulltime(fieldset.U.grid.time_full[-1])
 
-    # execute simulation
-    ctd_particleset.execute(
-        [_sample_salinity, _sample_temperature, _ctd_cast],
-        endtime=fieldset_endtime,
-        dt=outputdt,
-        output_file=out_file,
-    )
+    for ctd in ctds:
+        if ctd.spacetime.time < fieldset_starttime:
+            raise RuntimeError("CTD deployed before fieldset starts.")
+
+        # depth the ctd will go to. deepest between ctd max depth and bathymetry.
+        max_depth = min(
+            ctd.max_depth,
+            fieldset.bathymetry.eval(
+                z=0, y=ctd.spacetime.location.lat, x=ctd.spacetime.location.lon, time=0
+            ),
+        )
+
+        # define parcel particles
+        ctd_particleset = ParticleSet(
+            fieldset=fieldset,
+            pclass=_CTDParticle,
+            lon=[ctd.spacetime.location.lon],
+            lat=[ctd.spacetime.location.lat],
+            depth=[ctd.min_depth],
+            time=[ctd.spacetime.time],
+            max_depth=[max_depth],
+            winch_speed=[WINCH_SPEED],
+        )
+
+        # define output file for the simulation
+        out_file = ctd_particleset.ParticleFile(name=out_path, outputdt=outputdt)
+
+        # execute simulation
+        ctd_particleset.execute(
+            [_sample_salinity],  # , _sample_temperature, _ctd_cast],
+            endtime=fieldset_endtime,
+            dt=outputdt,
+            verbose_progress=False,
+            output_file=out_file,
+        )
+
+        if ctd_particleset.raising[0] == 0 or not np.isclose(
+            ctd_particleset.particledata.depth[0], ctd.min_depth
+        ):
+            raise RuntimeError(
+                "Simulation ended before CTD resurfaced. This most likely means the field time dimension did not match the simulation time span."
+            )
