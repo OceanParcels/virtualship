116e570f8fd964178e9452068225c0e13fa0b5b0
Aart Stuurman <aartstuurman@hotmail.com>
2024-07-02 12:43:55 +0200

    drifter test, wip

--- tests/instruments/test_drifters.py
+++ tests/instruments/test_drifters.py
@@ -1,36 +1,61 @@
 """Test the simulation of drifters."""
 
+import datetime
 from datetime import timedelta
 
 import numpy as np
+import py
 from parcels import FieldSet
 
 from virtual_ship import Location, Spacetime
 from virtual_ship.instruments.drifter import Drifter, simulate_drifters
 
 
-def test_simulate_drifters() -> None:
+def test_simulate_drifters(tmpdir: py.path.LocalPath) -> None:
+    # arbitrary time offset for the dummy fieldset
+    base_time = datetime.datetime.strptime("1950-01-01", "%Y-%m-%d")
+
+    v = np.full((2, 2, 2), 1.0)
+    u = np.full((2, 2, 2), 1.0)
+    t = np.full((2, 2, 2), 1.0)
+
     fieldset = FieldSet.from_data(
-        {"U": 0, "V": 0, "T": 0},
+        {"V": v, "U": u, "T": t},
         {
-            "lon": 0,
-            "lat": 0,
-            "time": [np.datetime64("1950-01-01") + np.timedelta64(632160, "h")],
+            "lon": np.array([0.0, 10.0]),
+            "lat": np.array([0.0, 10.0]),
+            "time": [
+                np.datetime64(base_time + datetime.timedelta(seconds=0)),
+                np.datetime64(base_time + datetime.timedelta(weeks=10)),
+            ],
         },
     )
 
-    min_depth = -fieldset.U.depth[0]
-
+    # drifters to deploy
     drifters = [
         Drifter(
-            spacetime=Spacetime(location=Location(latitude=0, longitude=0), time=0),
-            min_depth=min_depth,
+            spacetime=Spacetime(
+                location=Location(latitude=0, longitude=0),
+                time=base_time + datetime.timedelta(days=0),
+            ),
+            depth=0.0,
+            lifetime=datetime.timedelta(hours=2),  # TODO choose this
+            asd
         )
     ]
+    # TODO more drifters?
+    asdas
+
+    # perform simulation
+    out_path = tmpdir.join("out.zarr")
 
     simulate_drifters(
-        drifters=drifters,
         fieldset=fieldset,
-        out_file_name="test",
+        out_path=out_path,
+        drifters=drifters,
         outputdt=timedelta(minutes=5),
+        endtime=None,
     )
+
+    asdasd
+    # test ouput
--- virtual_ship/instruments/drifter.py
+++ virtual_ship/instruments/drifter.py
@@ -1,10 +1,17 @@
 """Drifter instrument."""
 
 from dataclasses import dataclass
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 import numpy as np
-from parcels import AdvectionRK4, FieldSet, JITParticle, ParticleSet, Variable
+import py
+from parcels import (
+    AdvectionRK4,
+    FieldSet,
+    JITParticle,
+    ParticleSet,
+    Variable,
+)
 
 from ..spacetime import Spacetime
 
@@ -14,12 +21,16 @@ class Drifter:
     """Configuration for a single Drifter."""
 
     spacetime: Spacetime
-    min_depth: float
+    depth: float  # depth at which it floats and samples
+    lifetime: timedelta | None  # if none, lifetime is infinite
 
 
 _DrifterParticle = JITParticle.add_variables(
     [
         Variable("temperature", dtype=np.float32, initial=np.nan),
+        Variable("has_lifetime", dtype=np.int8),  # bool
+        Variable("age", dtype=np.float32, initial=0.0),
+        Variable("lifetime", dtype=np.float32),
     ]
 )
 
@@ -28,53 +39,57 @@ def _sample_temperature(particle, fieldset, time):
     particle.temperature = fieldset.T[time, particle.depth, particle.lat, particle.lon]
 
 
-def _check_error(particle, fieldset, time):
-    if particle.state >= 50:  # This captures all Errors
-        particle.delete()
+def _check_lifetime(particle, fieldset, time):
+    if particle.has_lifetime == 1:
+        particle.age += particle.dt
+        if particle.age >= particle.lifetime:
+            particle.delete()
 
 
 def simulate_drifters(
-    drifters: list[Drifter],
     fieldset: FieldSet,
-    out_file_name: str,
+    out_path: str | py.path.LocalPath,
+    drifters: list[Drifter],
     outputdt: timedelta,
+    endtime: datetime | None,
 ) -> None:
     """
     Use parcels to simulate a set of drifters in a fieldset.
 
+    :param fieldset: The fieldset to simulate the CTDs in.
+    :param out_path: The path to write the results to.
     :param drifters: A list of drifters to simulate.
-    :param fieldset: The fieldset to simulate the drifters in.
-    :param out_file_name: The file to write the results to.
-    :param outputdt: Interval which dictates the update frequency of file output during simulation
+    :param outputdt: Interval which dictates the update frequency of file output during simulation.
+    :param endtime: Stop at this time, or if None, continue until the end of the fieldset or until all drifters ended. If this is earlier than the last drifter ended or later than the end of the fieldset, a warning will be printed.
     """
-    lon = [drifter.spacetime.location.lon for drifter in drifters]
-    lat = [drifter.spacetime.location.lat for drifter in drifters]
-    time = [drifter.spacetime.time for drifter in drifters]
-
     # define parcel particles
     drifter_particleset = ParticleSet(
         fieldset=fieldset,
         pclass=_DrifterParticle,
-        lon=lon,
-        lat=lat,
-        depth=[drifter.min_depth for drifter in drifters],
-        time=time,
+        lat=[drifter.spacetime.location.lat for drifter in drifters],
+        lon=[drifter.spacetime.location.lon for drifter in drifters],
+        depth=[drifter.depth for drifter in drifters],
+        time=[drifter.spacetime.time for drifter in drifters],
+        has_lifetime=[1 if drifter.lifetime is not None else 0 for drifter in drifters],
+        lifetime=[drifter.lifetime.total_seconds() for drifter in drifters],
     )
 
     # define output file for the simulation
-    out_file = drifter_particleset.ParticleFile(
-        name=out_file_name,
-        outputdt=outputdt,
-        chunks=(1, 500),
-    )
+    out_file = drifter_particleset.ParticleFile(name=out_path, outputdt=outputdt)
 
-    # get time when the fieldset ends
     fieldset_endtime = fieldset.time_origin.fulltime(fieldset.U.grid.time_full[-1])
+    if endtime is None or endtime > fieldset_endtime:
+        actual_endtime = fieldset_endtime
+    else:
+        actual_endtime = np.timedelta64(endtime)
+    asdas  # test endtime and stuff
+    asdasd  # test time offset for particles
+    asddsa  # warnings when times are weird
 
     # execute simulation
     drifter_particleset.execute(
-        [AdvectionRK4, _sample_temperature, _check_error],
-        endtime=fieldset_endtime,
+        [AdvectionRK4, _sample_temperature, _check_lifetime],
+        endtime=actual_endtime,
         dt=outputdt,
         output_file=out_file,
     )
