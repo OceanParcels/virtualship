9d6b2cf0b21f81d7e018b94e97b446a2e799cd1d
Aart Stuurman <aartstuurman@hotmail.com>
2024-07-18 14:32:39 +0200

    remove strict coordinates

--- virtual_ship/virtual_ship_configuration.py
+++ virtual_ship/virtual_ship_configuration.py
@@ -58,19 +58,6 @@ class VirtualShipConfig:
             raise ValueError("Route needs to consist of at least locations.")
 
         if not all(
-            [self._is_valid_location(coord) for coord in self.route_coordinates]
-        ):
-            raise ValueError("Invalid coordinates in route.")
-
-        if not all(
-            [
-                self._is_valid_location(coord)
-                for coord in self.argo_float_deploy_locations
-            ]
-        ):
-            raise ValueError("Argo float deploy locations are not valid coordinates.")
-
-        if not all(
             [
                 any(
                     [
@@ -87,11 +74,6 @@ class VirtualShipConfig:
             )
 
         if not all(
-            [self._is_valid_location(coord) for coord in self.drifter_deploy_locations]
-        ):
-            raise ValueError("Drifter deploy locations are not valid coordinates.")
-
-        if not all(
             [
                 any(
                     [
@@ -106,11 +88,6 @@ class VirtualShipConfig:
             raise ValueError(
                 "Drifter deploy locations are not exactly on route coordinates."
             )
-
-        if not all(
-            [self._is_valid_location(coord) for coord in self.ctd_deploy_locations]
-        ):
-            raise ValueError("CTD deploy locations are not valid coordinates.")
 
         if not all(
             [
@@ -145,12 +122,3 @@ class VirtualShipConfig:
 
         if self.adcp_config.max_depth > 0:
             raise ValueError("ADCP max depth must be negative.")
-
-    @staticmethod
-    def _is_valid_location(location: Location) -> bool:
-        return (
-            location.lat >= -90
-            and location.lat <= 90
-            and location.lon >= -180
-            and location.lon <= 180
-        )
