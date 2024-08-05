159235c71f20ba97e34746d4884c10927690c136
Aart Stuurman <aartstuurman@hotmail.com>
2024-08-05 16:16:29 +0200

    docstring improvements

--- virtual_ship/waypoint.py
+++ virtual_ship/waypoint.py
@@ -9,7 +9,7 @@ from .location import Location
 
 @dataclass
 class Waypoint:
-    """A Waypoint to sail to."""
+    """A Waypoint to sail to with an optional time and an optional instrument."""
 
     location: Location
     time: datetime | None = None
