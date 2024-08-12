d2ffd5ba9e55a99f5bd963f90f3cdb49ba423dcf
Aart Stuurman <aartstuurman@hotmail.com>
2024-08-12 18:00:47 +0200

    verify waypoints are on water (#48)
    * verify waypoints are on water
    
    * Make zero uv on land check a function
    
    * add test for sailship verify
    
    * comments

--- virtual_ship/sailship.py
+++ virtual_ship/sailship.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 from typing import Generator
 
 import pyproj
+from parcels import FieldSet
 from sortedcontainers import SortedList
 
 from .costs import costs
@@ -36,7 +37,7 @@ def sailship(config: VirtualShipConfig):
     # projection used to sail between waypoints
     projection = pyproj.Geod(ellps="WGS84")
 
-    _verify_waypoints(config.waypoints, config.ship_speed, projection=projection)
+    _verify_waypoints(projection=projection, config=config)
 
     # simulate the sailing and aggregate what measurements should be simulated
     schedule_results = _simulate_schedule(
@@ -386,22 +387,26 @@ def _argo_float_task(
 
 
 def _verify_waypoints(
-    waypoints: list[Waypoint], ship_speed: float, projection: pyproj.Geod
+    projection: pyproj.Geod,
+    config: VirtualShipConfig,
 ) -> None:
     """
     Verify waypoints are ordered by time, first waypoint has a start time, and that schedule is feasible in terms of time if no unexpected events happen.
 
-    :param waypoints: The waypoints to check.
-    :param ship_speed: Speed of the ship.
     :param projection: projection used to sail between waypoints.
+    :param config: The cruise configuration.
     :raises PlanningError: If waypoints are not feasible or incorrect.
+    :raises ValueError: If there are no fieldsets in the config, which are needed to verify all waypoints are on water.
     """
+    if len(config.waypoints) == 0:
+        raise PlanningError("At least one waypoint must be provided.")
+
     # check first waypoint has a time
-    if waypoints[0].time is None:
+    if config.waypoints[0].time is None:
         raise PlanningError("First waypoint must have a specified time.")
 
     # check waypoint times are in ascending order
-    timed_waypoints = [wp for wp in waypoints if wp.time is not None]
+    timed_waypoints = [wp for wp in config.waypoints if wp.time is not None]
     if not all(
         [
             next.time >= cur.time
@@ -412,9 +417,47 @@ def _verify_waypoints(
             "Each waypoint should be timed after all previous waypoints"
         )
 
+    # check if all waypoints are in water
+    # this is done by picking an arbitrary provided fieldset and checking if UV is not zero
+
+    # get all available fieldsets
+    available_fieldsets = [
+        fs
+        for fs in [
+            config.adcp_config.fieldset if config.adcp_config is not None else None,
+            config.argo_float_config.fieldset,
+            config.ctd_config.fieldset,
+            config.drifter_config.fieldset,
+            (
+                config.ship_underwater_st_config.fieldset
+                if config.ship_underwater_st_config is not None
+                else None
+            ),
+        ]
+        if fs is not None
+    ]
+    # check if there are any fieldsets, else its an error
+    if len(available_fieldsets) == 0:
+        raise ValueError(
+            "No fieldsets provided to check if waypoints are on land. Assuming no provided fieldsets is an error."
+        )
+    # pick any
+    fieldset = available_fieldsets[0]
+    # get waypoints with 0 UV
+    land_waypoints = [
+        (wp_i, wp)
+        for wp_i, wp in enumerate(config.waypoints)
+        if _is_on_land_zero_uv(fieldset, wp)
+    ]
+    # raise an error if there are any
+    if len(land_waypoints) > 0:
+        raise PlanningError(
+            f"The following waypoints are on land: {['#' + str(wp_i) + ' ' + str(wp) for (wp_i, wp) in land_waypoints]}"
+        )
+
     # check that ship will arrive on time at each waypoint (in case no unexpected event happen)
-    time = waypoints[0].time
-    for wp_i, (wp, wp_next) in enumerate(zip(waypoints, waypoints[1:])):
+    time = config.waypoints[0].time
+    for wp_i, (wp, wp_next) in enumerate(zip(config.waypoints, config.waypoints[1:])):
         if wp.instrument is InstrumentType.CTD:
             time += timedelta(minutes=20)
 
@@ -423,7 +466,7 @@ def _verify_waypoints(
         )
         distance = geodinv[2]
 
-        time_to_reach = timedelta(seconds=distance / ship_speed)
+        time_to_reach = timedelta(seconds=distance / config.ship_speed)
         arrival_time = time + time_to_reach
 
         if wp_next.time is None:
@@ -434,3 +477,16 @@ def _verify_waypoints(
             )
         else:
             time = wp_next.time
+
+
+def _is_on_land_zero_uv(fieldset: FieldSet, waypoint: Waypoint) -> bool:
+    """
+    Check if waypoint is on land by assuming zero velocity means land.
+
+    :param fieldset: The fieldset to sample the velocity from.
+    :param waypoint: The waypoint to check.
+    :returns: If the waypoint is on land.
+    """
+    return fieldset.UV.eval(
+        0, 0, waypoint.location.lat, waypoint.location.lon, applyConversion=False
+    ) == (0.0, 0.0)
