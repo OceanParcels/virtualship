5091a7db51a8a56e5346d2b80272bc02103ec2b7
Aart Stuurman <aartstuurman@hotmail.com>
2024-08-19 13:36:19 +0000

    ADCP make realistic (#57)
    Conversion of ADCP zarr to CSV-like file to make experience more realistic.
    Dummy function that can be filled in later to add noise. We don't add noise now because we want to focus on different parts of the software

--- virtual_ship/make_realistic/__init__.py
+++ virtual_ship/make_realistic/__init__.py
@@ -1,5 +1,6 @@
 """Conversion functions from zarr instrument results to realistic file formats with data adjusted to be more realistic."""
 
+from .adcp_make_realistic import adcp_make_realistic
 from .ctd_make_realistic import ctd_make_realistic
 
-__all__ = ["ctd_make_realistic"]
+__all__ = ["adcp_make_realistic", "ctd_make_realistic"]
