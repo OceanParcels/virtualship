60645d26531ab36690be0b34190c3071ecf3f1b2
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2024-09-26 14:04:32 +0200

    add version information

--- virtual_ship/__init__.py
+++ virtual_ship/__init__.py
@@ -1,9 +1,18 @@
 """Code for the Virtual Ship Classroom, where Marine Scientists can combine Copernicus Marine Data with an OceanParcels ship to go on a virtual expedition."""
 
+from importlib.metadata import version as _version
+
 from .location import Location
 from .spacetime import Spacetime
+
+try:
+    __version__ = _version("virtual_ship")
+except Exception:
+    # Local copy or not installed with setuptools
+    __version__ = "unknown"
 
 __all__ = [
     "Location",
     "Spacetime",
+    "__version__",
 ]
