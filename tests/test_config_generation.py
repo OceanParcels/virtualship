849c05b0a045681a86985a9ec62697cddfa95765
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2024-10-21 10:29:31 +0200

    Add validation of example configs

--- tests/test_config_generation.py
+++
@@ -1,9 +0,0 @@
-from virtualship.utils import get_example_config, get_example_schedule
-
-
-def test_get_example_config():
-    assert len(get_example_config()) > 0
-
-
-def test_get_example_schedule():
-    assert len(get_example_schedule()) > 0
---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+from virtualship.expedition import Schedule, ShipConfig
+from virtualship.utils import get_example_config, get_example_schedule
+
+
+def test_get_example_config():
+    assert len(get_example_config()) > 0
+
+
+def test_get_example_schedule():
+    assert len(get_example_schedule()) > 0
+
+
+def test_valid_example_config(tmp_path):
+    path = tmp_path / "ship_config.yaml"
+    with open(path, "w") as file:
+        file.write(get_example_config())
+
+    ShipConfig.from_yaml(path)
+
+
+def test_valid_example_schedule(tmp_path):
+    path = tmp_path / "schedule.yaml"
+    with open(path, "w") as file:
+        file.write(get_example_schedule())
+
+    Schedule.from_yaml(path)
